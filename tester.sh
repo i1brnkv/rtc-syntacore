389a3c84c1f7a4667ee981cf4f686ccdaa38cffe
Ivan Bornyakov <NULL>
2018-05-14 23:14:00 +0300

    tester: add test for time speed greater than 1

--- tester.sh
+++ tester.sh
@@ -81,6 +81,26 @@ slow_speed_test()
 	echo
 }
 
+fast_speed_test()
+{
+	echo -e " ${GC}*${NC} trying to set time speed to 1.6"
+	echo 1.6 > /proc/rtc-syntacore/speed
+	typeset -i T1=`cat /sys/class/rtc/${DEV}/since_epoch`
+	for i in `seq 10`
+	do
+		echo -n "$(( 11 - ${i} ))..."
+		sleep 1
+	done
+	typeset -i T2=`cat /sys/class/rtc/${DEV}/since_epoch`
+	TDIF=$(( ${T2} - ${T1} ))
+	echo
+	echo "in 10 real seconds left ${TDIF} seconds"
+	[ ${TDIF} -eq 16 ]
+	echo -e " ${GC}OK${NC}"
+	echo
+
+}
+
 cleanup()
 {
 	if [ $? -ne 0 ]
@@ -111,3 +131,4 @@ hwclock_get_test
 timevalid_test
 hwclock_set_test
 slow_speed_test
+fast_speed_test
