098f6ffabeacabafeed3cd92befd8f5e7cc3d54c
Ivan Bornyakov <NULL>
2018-05-14 22:09:06 +0300

    tester: add test for inserting module

--- tester.sh
+++ tester.sh
@@ -16,5 +16,20 @@ build_test()
 	cd ../
 }
 
+insmod_test()
+{
+	echo " * trying to insert module "
+	insmod ./src/rtc-syntacore.ko
+	if [ -n "`lsmod | grep rtc_syntacore`" ]
+	then
+		echo " OK"
+	else
+		echo " FAIL"
+		exit 1
+	fi
+	echo
+}
+
 cd `dirname $0`
 build_test
+insmod_test
